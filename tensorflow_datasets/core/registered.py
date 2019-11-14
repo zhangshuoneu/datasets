c5f72410c4b835a02c8591a91e7ed5036f2daf59
TensorFlow Datasets Team <no-reply@google.com>
2019-11-14 08:58:31 -0800

    Internal
    PiperOrigin-RevId: 280439800

--- tensorflow_datasets/core/registered.py
+++ tensorflow_datasets/core/registered.py
@@ -235,66 +235,6 @@ def load(name,
       `tfds.Split.TEST`).
     data_dir: `str` (optional), directory to read/write data.
       Defaults to "~/tensorflow_datasets".
-    batch_size: `int`, if set, add a batch dimension to examples. Note that
-      variable length features will be 0-padded. If
-      `batch_size=-1`, will return the full dataset as `tf.Tensor`s.
-    in_memory: `bool`, if `True`, loads the dataset in memory which
-      increases iteration speeds. Note that if `True` and the dataset has
-      unknown dimensions, the features will be padded to the maximum
-      size across the dataset.
-    shuffle_files: `bool`, whether to shuffle the input files.
-      Defaults to `False`.
-    download: `bool` (optional), whether to call
-      `tfds.core.DatasetBuilder.download_and_prepare`
-      before calling `tf.DatasetBuilder.as_dataset`. If `False`, data is
-      expected to be in `data_dir`. If `True` and the data is already in
-      `data_dir`, `download_and_prepare` is a no-op.
-    as_supervised: `bool`, if `True`, the returned `tf.data.Dataset`
-      will have a 2-tuple structure `(input, label)` according to
-      `builder.info.supervised_keys`. If `False`, the default,
-      the returned `tf.data.Dataset` will have a dictionary with all the
-      features.
-    decoders: Nested dict of `Decoder` objects which allow to customize the
-      decoding. The structure should match the feature structure, but only
-      customized feature keys need to be present. See
-      [the guide](https://github.com/tensorflow/datasets/tree/master/docs/decode.md)
-      for more info.
-    with_info: `bool`, if True, tfds.load will return the tuple
-      (tf.data.Dataset, tfds.core.DatasetInfo) containing the info associated
-      with the builder.
-    builder_kwargs: `dict` (optional), keyword arguments to be passed to the
-      `tfds.core.DatasetBuilder` constructor. `data_dir` will be passed
-      through by default.
-    download_and_prepare_kwargs: `dict` (optional) keyword arguments passed to
-      `tfds.core.DatasetBuilder.download_and_prepare` if `download=True`. Allow
-      to control where to download and extract the cached data. If not set,
-      cache_dir and manual_dir will automatically be deduced from data_dir.
-    as_dataset_kwargs: `dict` (optional), keyword arguments passed to
-      `tfds.core.DatasetBuilder.as_dataset`.
-    try_gcs: `bool`, if True, tfds.load will see if the dataset exists on
-      the public GCS bucket before building it locally.
-
-  Returns:
-    ds: `tf.data.Dataset`, the dataset requested, or if `split` is None, a
-      `dict<key: tfds.Split, value: tfds.data.Dataset>`. If `batch_size=-1`,
-      these will be full datasets as `tf.Tensor`s.
-    ds_info: `tfds.core.DatasetInfo`, if `with_info` is True, then `tfds.load`
-      will return a tuple `(ds, ds_info)` containing dataset information
-      (version, features, splits, num_examples,...). Note that the `ds_info`
-      object documents the entire dataset, regardless of the `split` requested.
-      Split-specific information is available in `ds_info.splits`.
-  """
-  # pylint: enable=line-too-long
-
-  name, name_builder_kwargs = _dataset_name_and_kwargs_from_name_str(name)
-  name_builder_kwargs.update(builder_kwargs or {})
-  builder_kwargs = name_builder_kwargs
-
-  # Set data_dir
-  if try_gcs and gcs_utils.is_dataset_on_gcs(name):
-    data_dir = constants.GCS_DATA_DIR
-  elif data_dir is None:
-    data_dir = constants.DATA_DIR
 
   dbuilder = builder(name, data_dir=data_dir, **builder_kwargs)
   if download:
